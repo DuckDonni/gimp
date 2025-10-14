63f8cf75fe97192951bd2ae4baf401fcc6d843be
DuckDonni <jonahsmith2010@gmail.com>
2025-10-14 15:21:02 -0400

    Added Temporary Slider

--- app/widgets/gimpstyluseditor.c
+++ app/widgets/gimpstyluseditor.c
@@ -10,16 +10,18 @@
 #include "core/gimp.h"
 #include "core/gimpcontext.h"
 
-#include "gimpdocked.h"
-#include "gimpdockable.h"
 #include "gimpdataeditor.h"
+#include "gimpdockable.h"
+#include "gimpdocked.h"
 #include "gimpuimanager.h"
 
 #include "gimp-intl.h"
 
 #include "gimpstyluseditor.h"
 
-static void   stylus_editor_constructed (GObject *object);
+static void stylus_editor_constructed (GObject *object);
+static void stylus_editor_dispose (GObject *object);
+static void stylus_editor_slider_changed (GtkAdjustment *adjustment, StylusEditor *editor);
 
 G_DEFINE_TYPE (StylusEditor, stylus_editor, GIMP_TYPE_EDITOR)
 
@@ -32,38 +34,86 @@ stylus_editor_class_init (StylusEditorClass *klass)
   GimpDataEditorClass *editor_class = GIMP_DATA_EDITOR_CLASS (klass);
 
   object_class->constructed = stylus_editor_constructed;
-  editor_class->title       = _("Stylus Editor");
+  object_class->dispose     = stylus_editor_dispose;
+  editor_class->title       = _ ("Stylus Editor");
 }
 
 static void
-stylus_editor_init (StylusEditor *dock)
+stylus_editor_init (StylusEditor *editor)
 {
-  // BLANK (IE NO UI)
+  /* Initialize slider components */
+  editor->slider_adjustment = NULL;
+  editor->slider_scale      = NULL;
+}
+
+static void
+stylus_editor_dispose (GObject *object)
+{
+  StylusEditor *editor = STYLUS_EDITOR (object);
+
+  if (editor->slider_adjustment)
+    {
+      g_object_unref (editor->slider_adjustment);
+      editor->slider_adjustment = NULL;
+    }
+
+  G_OBJECT_CLASS (parent_class)->dispose (object);
 }
 
 static void
 stylus_editor_constructed (GObject *object)
 {
+  StylusEditor *editor = STYLUS_EDITOR (object);
+  GtkWidget    *vbox;
+  GtkWidget    *frame;
+  GtkWidget    *scale;
+
   G_OBJECT_CLASS (parent_class)->constructed (object);
 
-  /* Optional: hide the button bar to keep it visually blank */
+  // Create main container
+  vbox = gtk_box_new (GTK_ORIENTATION_VERTICAL, 12);
+  gtk_container_add (GTK_CONTAINER (editor), vbox);
+  gtk_widget_show (vbox);
+
+  frame = gimp_frame_new (_ ("Stylus Settings"));
+  gtk_box_pack_start (GTK_BOX (vbox), frame, FALSE, FALSE, 0);
+  gtk_widget_show (frame);
+
+  editor->slider_adjustment = gtk_adjustment_new (50.0, 0.0, 100.0, 1.0, 10.0, 0.0);
+
+  scale = gimp_spin_scale_new (editor->slider_adjustment, _ ("Pressure Sensitivity"), 1);
+  gimp_spin_scale_set_constrain_drag (GIMP_SPIN_SCALE (scale), TRUE);
+
+  gtk_container_add (GTK_CONTAINER (frame), scale);
+  gtk_widget_show (scale);
+
+  editor->slider_scale = scale;
+
+  // Connect signal for slider changes
+  g_signal_connect (editor->slider_adjustment, "value-changed",
+                    G_CALLBACK (stylus_editor_slider_changed), editor);
+
   gimp_docked_set_show_button_bar (GIMP_DOCKED (object), FALSE);
+}
+
+static void
+stylus_editor_slider_changed (GtkAdjustment *adjustment, StylusEditor *editor)
+{
+  // Function that happens when slider value is changed
+  gdouble value = gtk_adjustment_get_value (adjustment);
+
+  g_print ("Stylus slider value changed to: %.1f\n", value);
 }
 
 /* Public functions */
 
 GtkWidget *
-stylus_editor_new (GimpContext *context,
-               GimpMenuFactory *menu_factory)
+stylus_editor_new (GimpContext *context, GimpMenuFactory *menu_factory)
 {
   g_return_val_if_fail (GIMP_IS_CONTEXT (context), NULL);
 
-  GtkWidget *dock = g_object_new (STYLUS_TYPE_DOCK,
-    
-                                  "context",         context,
-                                  
-                                  "menu-identifier", "<StylusEditor>",
-                                  NULL);
+  GtkWidget *dock = g_object_new (STYLUS_TYPE_DOCK, "context", context,
+                                  "menu-identifier", "<StylusEditor>", NULL);
 
   return dock;
 }
--- app/widgets/gimpstyluseditor.h
+++ app/widgets/gimpstyluseditor.h
@@ -1,35 +1,43 @@
-    #ifndef __STYLUS_EDITOR_H__
-    #define __STYLUS_EDITOR_H__
+#ifndef __STYLUS_EDITOR_H__
+#define __STYLUS_EDITOR_H__
 
-    #include "gimpeditor.h"
+#include "gimpeditor.h"
 
-    G_BEGIN_DECLS
+G_BEGIN_DECLS
 
-    #define STYLUS_TYPE_DOCK            (stylus_editor_get_type ())
-    #define STYLUS_EDITOR(obj)            (G_TYPE_CHECK_INSTANCE_CAST ((obj), STYLUS_EDITOR_DOCK, StylusEditor))
-    #define STYLUS_EDITOR_CLASS(klass)    (G_TYPE_CHECK_CLASS_CAST ((klass), STYLUS_EDITOR_DOCK, StylusEditorClass))
-    #define STYLUS_IS_DOCK(obj)         (G_TYPE_CHECK_INSTANCE_TYPE ((obj), STYLUS_EDITOR_DOCK))
-    #define STYLUS_IS_DOCK_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE ((klass), STYLUS_EDITOR_DOCK))
-    #define STYLUS_EDITOR_GET_CLASS(obj)  (G_TYPE_INSTANCE_GET_CLASS ((obj), STYLUS_EDITOR_DOCK, StylusEditorClass))
+#define STYLUS_TYPE_DOCK (stylus_editor_get_type ())
+#define STYLUS_EDITOR(obj) \
+  (G_TYPE_CHECK_INSTANCE_CAST ((obj), STYLUS_EDITOR_DOCK, StylusEditor))
+#define STYLUS_EDITOR_CLASS(klass) \
+  (G_TYPE_CHECK_CLASS_CAST ((klass), STYLUS_EDITOR_DOCK, StylusEditorClass))
+#define STYLUS_IS_DOCK(obj) \
+  (G_TYPE_CHECK_INSTANCE_TYPE ((obj), STYLUS_EDITOR_DOCK))
+#define STYLUS_IS_DOCK_CLASS(klass) \
+  (G_TYPE_CHECK_CLASS_TYPE ((klass), STYLUS_EDITOR_DOCK))
+#define STYLUS_EDITOR_GET_CLASS(obj) \
+  (G_TYPE_INSTANCE_GET_CLASS ((obj), STYLUS_EDITOR_DOCK, StylusEditorClass))
 
-    typedef struct _StylusEditor        StylusEditor;
-    typedef struct _StylusEditorClass   StylusEditorClass;
+typedef struct _StylusEditor      StylusEditor;
+typedef struct _StylusEditorClass StylusEditorClass;
 
-    struct _StylusEditor
-    {
-    GimpEditor  parent_instance;
-    };
+struct _StylusEditor
+{
+  GimpEditor parent_instance;
 
-    struct _StylusEditorClass
-    {
-    GimpEditorClass  parent_class;
-    };
+  // Slider Components
+  GtkAdjustment *slider_adjustment;
+  GtkWidget     *slider_scale;
+};
 
-    GType       stylus_editor_get_type (void) G_GNUC_CONST;
+struct _StylusEditorClass
+{
+  GimpEditorClass parent_class;
+};
 
-    GtkWidget * stylus_editor_new      (GimpContext     *context,
-                                    GimpMenuFactory *menu_factory);
+GType stylus_editor_get_type (void) G_GNUC_CONST;
 
-    G_END_DECLS
+GtkWidget *stylus_editor_new (GimpContext *context, GimpMenuFactory *menu_factory);
 
-    #endif /* __STYLUS_EDITOR_H__ */
+G_END_DECLS
+
+#endif /* __STYLUS_EDITOR_H__ */
