ef60c97630945e311c98363aaad62bc4d5d1ee0b
DuckDonni <jonahsmith2010@gmail.com>
2025-10-15 11:33:05 -0400

    format adjustments

--- app/widgets/gimpstyluseditor.h
+++ app/widgets/gimpstyluseditor.h
@@ -6,16 +6,11 @@
 G_BEGIN_DECLS
 
 #define STYLUS_TYPE_DOCK (stylus_editor_get_type ())
-#define STYLUS_EDITOR(obj) \
-  (G_TYPE_CHECK_INSTANCE_CAST ((obj), STYLUS_EDITOR_DOCK, StylusEditor))
-#define STYLUS_EDITOR_CLASS(klass) \
-  (G_TYPE_CHECK_CLASS_CAST ((klass), STYLUS_EDITOR_DOCK, StylusEditorClass))
-#define STYLUS_IS_DOCK(obj) \
-  (G_TYPE_CHECK_INSTANCE_TYPE ((obj), STYLUS_EDITOR_DOCK))
-#define STYLUS_IS_DOCK_CLASS(klass) \
-  (G_TYPE_CHECK_CLASS_TYPE ((klass), STYLUS_EDITOR_DOCK))
-#define STYLUS_EDITOR_GET_CLASS(obj) \
-  (G_TYPE_INSTANCE_GET_CLASS ((obj), STYLUS_EDITOR_DOCK, StylusEditorClass))
+#define STYLUS_EDITOR(obj) (G_TYPE_CHECK_INSTANCE_CAST ((obj), STYLUS_EDITOR_DOCK, StylusEditor))
+#define STYLUS_EDITOR_CLASS(klass) (G_TYPE_CHECK_CLASS_CAST ((klass), STYLUS_EDITOR_DOCK, StylusEditorClass))
+#define STYLUS_IS_DOCK(obj) (G_TYPE_CHECK_INSTANCE_TYPE ((obj), STYLUS_EDITOR_DOCK))
+#define STYLUS_IS_DOCK_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE ((klass), STYLUS_EDITOR_DOCK))
+#define STYLUS_EDITOR_GET_CLASS(obj) (G_TYPE_INSTANCE_GET_CLASS ((obj), STYLUS_EDITOR_DOCK, StylusEditorClass))
 
 typedef struct _StylusEditor      StylusEditor;
 typedef struct _StylusEditorClass StylusEditorClass;
